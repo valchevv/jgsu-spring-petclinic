beee437500997cae43d6255696e65b79ddb6e14f
Vladimir Valchev <v.vulchev@gmail.com>
2021-01-19 00:29:54 +0200

    read

--- pipeline.groovy
+++ pipeline.groovy
@@ -24,8 +24,7 @@ pipeline {
 
                 // Run Maven on a Unix agent.
                 //sh "mvn -Dmaven.test.failure.ignore=true clean package"
-                // sh './mvnw clean compile'
-                sh './good.sh'
+                 sh './mvnw clean compile'
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
